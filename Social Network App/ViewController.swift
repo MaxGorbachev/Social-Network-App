8c2ffda9516b5d0a4b38fc497c4d911110380e5a
Max Gorbachev <gorbachyov.maksim@mail.ru>
2021-12-23 21:08:55 +0300

    Initial Commit

---
+++ Social Network App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Network App
+//
+//  Created by Максим on 23.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
