73fe497916c814b06c45403cfc20f7b71a390555
MaxG <gorbachyov.maksim@mail.ru>
2021-12-30 02:51:21 +0300

    Refactor LoginView
    Optimization

--- Social Network App/LoginViewController.swift
+++ Social Network App/LoginViewController.swift
@@ -1,16 +1,27 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Social Network App
 //
-//  Created by Максим on 23.12.2021.
+//  Created by Максим on 30.12.2021.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        guard let login = loginTextField.text,
+              let password = passwordTextField.text else {return}
+
+        if login == "root",
+           password == "12345" {
+            print("Success")
+        } else {
+            print("Login error")
+        }
     }
 
+}
