2406a83a11f6d9c69148130df7191998252c15cc
MaxG <gorbachyov.maksim@mail.ru>
2021-12-30 03:38:51 +0300

    Fixed ScrollView

--- Social Network App/LoginViewController.swift
+++ Social Network App/LoginViewController.swift
@@ -3,25 +3,43 @@
 //  Social Network App
 //
 //  Created by Максим on 30.12.2021.
-//
 
 import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+
+    }
+
+    @objc func keyboardDidShown (_ notification: Notification) {
+        guard let keyboardHeigt = ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue)?.height else {return}
+
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeigt + 150, right: 0)
+        scrollView.contentInset = insets
+        scrollView.scrollIndicatorInsets = insets
+    }
+
+    
     @IBAction func loginButtonPressed(_ sender: Any) {
-        guard let login = loginTextField.text,
-              let password = passwordTextField.text else {return}
+        guard let login = loginField.text,
+              let password = passwordField.text else {return}
 
         if login == "root",
            password == "12345" {
-            print("Success")
+            print("Login succeed")
         } else {
-            print("Login error")
+            print("Login failed")
         }
     }
+
 
 }
